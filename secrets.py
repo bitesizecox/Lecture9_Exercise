52773fa5e009dbc60c0ee6c50ee5a97c9b289982
Meaghan Cox <mkcox@umich.edu>
2018-02-03 23:06:59 -0500

    updating secrets.py with access tokens

--- secrets.py
+++ secrets.py
@@ -2,3 +2,6 @@
 
 api_key = "sRmmzHOXJ9G8c9ajRnIfFD6b6"
 api_secret = "rcEtUi9lg3fogPENxyGLVkzLIClDgMVsjJAbQKWNQp2BDStiC4"
+
+access_token = "1201000536-1Zm8lBf2mTQm1oVqchy1npccVxAnygsN1jSZLMk"
+access_token_secret = "inpPAA7x5iGK8sL2AhGxAQe26tfSevA9XQvQnr9ohwdyw"

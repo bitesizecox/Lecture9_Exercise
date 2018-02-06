543798d89bbea52de7a27e0348962615154c5923
Meaghan Cox <mkcox@umich.edu>
2018-02-06 14:06:38 -0500

    Adding updated pauth.py to reflect expectations of lecture exercise 10

--- twitter-oauth.py
+++ twitter-oauth.py
@@ -1,6 +1,7 @@
 
 from requests_oauthlib import OAuth1Session
 import secrets
+import json
 
 client_key = secrets.api_key
 client_secret = secrets.api_secret
@@ -18,4 +19,10 @@ oauth = OAuth1Session(client_key,
 protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
 params = {'q':'food'}
 r = oauth.get(protected_url, params=params)
-print (r.text)
+# print (r.text)
+returned_data_json = json.loads(r.text)
+# print(returned_data_json)
+for s in returned_data_json['statuses']:
+	print(s['user']['name'])
+	print(s['text'])
+	print("-------------------------------------")
